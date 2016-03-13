30b9d96b4e2dfe2e50533a8da1dbf499f0d38ebb
Li Yerong <yerong@Lis-MacBook-Pro.local>
2016-03-12 17:52:33 -0800

    some cleanups

--- figure/visualization.py
+++ figure/visualization.py
@@ -2,16 +2,8 @@ from graphviz import Digraph
 import numpy as np
 
 f = Digraph('Markov_Chain_Diagram', filename='Markov', format = 'png')
-f.body.extend(['rankdir=LR', 'size="10, 5"'])
+f.body.extend(['rankdir=LR', 'size="10, 10"'])
 
-A = np.array([[1, 2, 3, 4, 0, 6,7],\
-		[1, 2, 3, 4, 0, 6, 1],\
-		[1, 2, 3, 4, 0, 6, 1],\
-		[1, 2, 3, 4, 0, 6, 1],\
-		[1, 2, 3, 4, 0, 6, 1],\
-		[0, 0, 0, 0, 0, 0, 1],\
-		[0, 0, 0, 0, 0, 0, 1]\
-	])
 A = np.loadtxt('../model/modelnhidden5groupAtrans.txt')
 f.attr('node', shape='doublecircle')
 f.node('START')
@@ -23,15 +15,15 @@ f.attr('node', shape='circle')
 
 for it in range(DIM):
 	if(A[-2,it]>1e-4): # plot the edge only the probability is greater than 1e-4
-		f.edge('START', '{0}'.format(it), label= str(A[-2, it]))
+		f.edge('START', '{0}'.format(it), label= str(round(A[-2, it], 3)))
 
 for it in range(DIM):
 	if(A[it,-1]>1e-4):
-		f.edge('{0}'.format(it), 'END', label= str(A[it, -1]))
+		f.edge('{0}'.format(it), 'END', label= str(round(A[it, -1], 3)))
 
 for outer_it in range(DIM):
 		for inner_it in range(DIM):
 			if(A[outer_it,inner_it]>1e-4):
-				f.edge('{0}'.format(outer_it), '{0}'.format(inner_it), label= str(A[outer_it,inner_it]))
+				f.edge('{0}'.format(outer_it), '{0}'.format(inner_it), label= str(round(A[outer_it,inner_it], 3)))
 
 f.view()
