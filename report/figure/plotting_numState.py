3870ff3b2629d5ecf230e13a60424089905fb07f
Li Yerong <yerong@Lis-MacBook-Pro.local>
2016-03-14 10:18:57 -0700

    report : HMM

---
+++ report/figure/plotting_numState.py
@@ -0,0 +1,33 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import re
+
+def plotting(num_list, data, Pick = 0):
+	assert len(num_list) == len(data), 'len(num_list) == len(data) should hold'
+	assert Pick>= 0 and Pick<7, 'Pick>= 0 and Pick<7 should hold'
+	plt.plot(num_list, data[:, 2])
+	plt.ylabel('$log{P}$', fontsize = 20)
+	plt.xlabel('Number of States', fontsize = 15)
+	plt.savefig('./probability.png')
+	plt.show()
+	return
+
+def main():
+	num_list = [5, 10, 20, 40]
+	data = []
+	###for num in [5, 10, 20, 40]:
+	for num in num_list:
+			filename = '../../probability/'+'prob_num{0}'.format(num)+'.txt'
+			finalProb = []
+			with open(filename, 'r') as file:
+				for line in file:
+					if 2 == len(line): continue
+					lst = line.split()
+					data.append(float(lst[-1]))
+	data = np.array(data)
+	data = data.reshape((len(num_list), 7))
+	plotting(num_list, data)
+
+
+if __name__ == "__main__":
+    main()
